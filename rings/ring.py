1f1d4e1b4b54697222a0f5af7cc7dab8e482b71e
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-04-03 20:33:19 -0400

    Cleanning duplicate line. I guess I'm quite tired.

--- rings/ring.py
+++ rings/ring.py
@@ -162,7 +162,6 @@ USERABORT    =  6 # User requested end of minimization
         f = fitt.v_circular()
         f.set_p0(self.data['x'], self.data['y'],
                 self.input['u'], self.input['v'])
-        f = fitt.v_circular()
         op = optimize.fmin_tnc(f.cost, f.p0, fprime=None,  args=args, approx_grad=1,  bounds=None, epsilon=1e-08, scale=None, offset=None, messages=15, maxCGit=-1, maxfun=500, eta=-1, stepmx=0, accuracy=0, fmin=0, ftol=-1, xtol=-1, pgtol=-1, rescale=-1, disp=verbose)
 
         self.opt_stat = op[2]

32ac38b50d23b96834bc69ce5b08f601a9794e6b
Capt. Dr. Guilherme Castelao <guilherme@castelao.net>
2013-04-23 17:12:47 -0400

    Changing the range of the CL ring parameters.

--- bin/run_montecarlo.py
+++ bin/run_montecarlo.py
@@ -4,6 +4,8 @@
 
 """ Script to run Monte Carlo simulations with PyRings.
 
+    The misc directory is probably a better place for this code.
+
 """
 
 from datetime import datetime, timedelta
@@ -16,17 +18,18 @@ from rings.misc.montecarlo import *
 
 # ============================================================================
 cfg_base = {'ring':
-            {'omega0': [1.7e-5, 2.7e-5], 
+            {'omega0': [1.5e-5, 2.6e-5], #2.0e-5
                 'delta': [120e3, 200e3], #170e3, 
-                'alpha': 2.5,
-                'u_c': [-0.25, 0.25],
-                'v_c': [-0.25, 0.25],
+                'alpha': [3.0, 5.0], #4.8
+                'u_c': [-0.20, 0.20],
+                'v_c': [-0.20, 0.20],
                 'lat_t0': 8,
                 'lon_t0': -50},
             'montecarlo':{
+                'sampling_type': 'regulargrid',
                 'dt': 0,
-                'Vnoise_sigma': [0.0, 0.20],
-                'Nsamples': [25, 1000], 
+                'Vnoise_sigma': 0, #[0.0, 0.20],
+                'Nsamples': [10, 500], 
                 'Rlimit': [25e3, 300e3]
             }
         }
@@ -40,3 +43,6 @@ data.describe()
 
 store = pd.HDFStore('montecarlo.h5')
 store.append('all_snapshot', data)
+
+#data['Lc_err'] = (data['xc_err']**2 + data['yc_err']**2)**0.5
+#data['Vc_err'] = (data['uc_err']**2 + data['vc_err']**2)**0.5
